eb71338b5db093d6ff2eddb39682896ff417cd3e
Dylan Bruschi <dylanbruschi54@gmail.com>
2017-12-05 00:03:29 -0600

    First draft of calculator

---
+++ AppleCalculator/OperationType.swift
@@ -0,0 +1,17 @@
+//
+//  OperationType.swift
+//  AppleCalculator
+//
+//  Created by Dylan Bruschi on 12/4/17.
+//  Copyright © 2017 Dylan Bruschi. All rights reserved.
+//
+
+import Foundation
+
+enum OperationType {
+    case none
+    case addition
+    case subtraction
+    case multiplication
+    case division
+}
